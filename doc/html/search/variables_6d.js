dc47ad283b3e60f0f63ccb747a4c6fe9f6d11c0f
Jim Evins <evins@snaught.com>
2013-09-01 20:59:26 -0400

    Imported latest version of doxygen generated manual.

--- doc/html/search/variables_6d.js
+++ doc/html/search/variables_6d.js
@@ -4,7 +4,6 @@ var searchData=
   ['m_5fdata_5fvalid_5fflag',['m_data_valid_flag',['../structglbarcode_1_1Barcode_1_1PrivateData.html#a6eee2f61d88bcceb695c5fd3ca2c9f55',1,'glbarcode::Barcode::PrivateData']]],
   ['m_5fempty_5fflag',['m_empty_flag',['../structglbarcode_1_1Barcode_1_1PrivateData.html#ad307453c05929503722656c4f980f4e4',1,'glbarcode::Barcode::PrivateData']]],
   ['m_5fend_5fbars_5fmodules',['m_end_bars_modules',['../classglbarcode_1_1BarcodeUpcBase.html#a1169d0986a2ef25d5bdaa8152bf6a25b',1,'glbarcode::BarcodeUpcBase']]],
-  ['m_5fend_5fbars_5fthresh',['m_end_bars_thresh',['../classglbarcode_1_1BarcodeUpcBase.html#ab4ccdf8a7e7f16d0397a2cd675477f4d',1,'glbarcode::BarcodeUpcBase']]],
   ['m_5ffirst_5fdigit_5fval',['m_first_digit_val',['../classglbarcode_1_1BarcodeUpcBase.html#af84745516d9a0bb67e440629cef5f78a',1,'glbarcode::BarcodeUpcBase']]],
   ['m_5fh',['m_h',['../structglbarcode_1_1Barcode_1_1PrivateData.html#a64023a671540a81692af2cbae7a98bb4',1,'glbarcode::Barcode::PrivateData']]],
   ['m_5fprimitives',['m_primitives',['../structglbarcode_1_1Barcode_1_1PrivateData.html#ab68d60a0819113fdf4d37ccb0c6e978e',1,'glbarcode::Barcode::PrivateData']]],
