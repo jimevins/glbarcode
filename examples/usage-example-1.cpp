e399ded8ac2e5d38db2e9dd45569d4f5f2d49bf4
Jim Evins <evins@snaught.com>
2014-08-20 22:21:55 -0400

    Documentation updates.

--- examples/usage-example-1.cpp
+++ examples/usage-example-1.cpp
@@ -1,22 +1,22 @@
 #include <glbarcode/Factory.h>
 #include <glbarcode/RendererSvg.h>
-
+ 
 using namespace glbarcode;
-
+ 
 int main( int argc, char **argv )
 {
 	/* Initialize barcode factory. */
         Factory::init();
-
+ 
         /* Create barcode object. */
         Barcode *bc = Factory::createBarcode( "code39" );
-
+ 
 	/* Set barcode options to show text and generate check digit, and then build */
 	bc->setChecksum(true).setShowText(true).build( "EXAMPLE-123", 288, 72 );
-
+ 
         /* Render barcode. */
         bc->render( RendererSvg().setFilename( "Example.svg" ) );
-
+ 
 	/* Cleanup. */
         delete bc;
 }
